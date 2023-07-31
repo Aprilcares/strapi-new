e25b0191eb34c16f3e82ba9d782769085d9dad71
pradyumna surampudi <pradyumna.surampudi@gmail.com>
2023-07-31 16:30:09 +0200

    updated plugins

--- config/plugins.js
+++ config/plugins.js
@@ -4,18 +4,46 @@ module.exports = ({ env }) => ({
       jwtSecret: env('JWT_SECRET'),
       },
     },
-    upload: {
+    graphql: {
       config: {
-        provider: 'cloudinary',
-        providerOptions: {
-          cloud_name: env('CLOUDINARY_NAME'),
-          api_key: env('CLOUDINARY_KEY'),
-          api_secret: env('CLOUDINARY_SECRET'),
-        },
-        actionOptions: {
-          upload: {},
-          delete: {},
+        endpoint: '/graphql',
+        shadowCRUD: true,
+        playgroundAlways: true,
+        depthLimit: 10,
+        amountLimit: 100,
+        apolloServer: {
+          tracing: false,
+          introspection: true,
         },
       },
     },
+    upload: {
+      config: {
+        provider: 'strapi-provider-upload-supabase-storage',
+        providerOptions: {
+          apiUrl: env('SUPABASE_API_URL'),
+          apiKey: env('SUPABASE_API_KEY'),
+          bucket:env('SUPABASE_BUCKET'),
+          directory: env('SUPABASE_BUCKET')
+        },
+        actionOptions: {
+          upload: {},
+          uploadStream: {},
+        },
+      },
+    },
+    // upload: {
+    //   config: {
+    //     provider: 'cloudinary',
+    //     providerOptions: {
+    //       cloud_name: env('CLOUDINARY_NAME'),
+    //       api_key: env('CLOUDINARY_KEY'),
+    //       api_secret: env('CLOUDINARY_SECRET'),
+    //     },
+    //     actionOptions: {
+    //       upload: {},
+    //       delete: {},
+    //     },
+    //   },
+    // },
   });
